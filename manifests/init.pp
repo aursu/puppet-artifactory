a83004b28c32e5dd04e5efd48c07b6433ca2e070
Alexander Ursu <alexander.ursu@gmail.com>
2018-01-27 02:31:19 +0100

    Empty mldu;e frPDK om tlate

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# artifactory
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include artifactory
+class artifactory {
+}
