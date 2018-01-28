fbffb46291de07a182d06c7d7c5e3c59575e6a4f
Alexander Ursu <alexander.ursu@gmail.com>
2018-01-28 22:11:46 +0100

    Added class artifactory (based on puppetforge/docker but Hiera-based w/o support for non-RedHat distros)

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,4 +7,41 @@
 # @example
 #   include artifactory
 class artifactory {
+    Artifactory::PackageName
+            $package_name,
+    Artifactory::Version
+            $version,
+    Boolean $manage_package,
+    String  $repo_location,
+    Array[String]
+            $prerequired_packages,
+    Boolean $manage_os_users,
+    String  $artifactory_user,
+    String  $artifactory_group,
+    Boolean $manage_service,
+    Docker::Ensure
+            $service_ensure,
+    String  $service_name,
+    Boolean $service_hasstatus,
+    Boolean $service_hasrestart,
+    Optional[String]
+            $service_ovverides_config,
+    Optional[String]
+            $service_overrides_template,
+    Optional[String]
+            $service_config,
+    Optional[String]
+            $service_config_template,
+    Optional[String]
+            $storage_config,
+    Optional[String]
+            $storage_config_template,
+)
+{
+  contain 'artifactory::repos'
+  contain 'artifactory::install'
+  contain 'artifactory::config'
+  contain 'artifactory::service'
+
+  Class['artifactory::repos'] -> Class['artifactory::install'] -> Class['artifactory::config'] ~> Class['artifactory::service']
 }
