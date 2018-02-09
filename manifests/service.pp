fce51c821de646b0a6e0a25cb94bcbee0e30e05b
Alexander Ursu <alexander.ursu@gmail.com>
2018-02-09 18:22:11 +0100

    Added File resource for setenv.sh in order to have ability to manage Artifactory from Puppet (script which loacted in TOMCAT_HOME/bin and consists custom ENV variables)

--- manifests/service.pp
+++ manifests/service.pp
@@ -12,6 +12,7 @@ class artifactory::service (
     String  $service_systemd_template   = $artifactory::service_systemd_template,
     String  $service_config             = $artifactory::service_config,
     String  $service_config_template    = $artifactory::service_config_template,
+    String  $service_setenv_template    = $artifactory::service_setenv_template,
     String  $artifactory_user           = $artifactory::artifactory_user,
     String  $artifactory_group          = $artifactory::artifactory_group,
     String  $artifactory_home           = $artifactory::params::artifactory_home,
@@ -44,16 +45,25 @@ class artifactory::service (
     include lsys::systemd
 
     File {
-        owner   => '0',
-        group   => '0',
-        mode    => '0644',
+        owner   => root,
+        group   => root,
+        mode    => 'u=rw,go=r',
     }
 
     if $::is_init_systemd {
+        # replace distributed with Artifactory setenv.sh file with custom
+        # considering that Artifactory is managed by Puppet on systemd running
+        # system
+        file { "${tomcat_home}/bin/setenv.sh"
+            content => template($service_config_template),
+        }
+
+        # environment file (or sysconfig file)
         file { $service_config:
             content => template($service_config_template),
         }
 
+        # systemd unit
         file { "/usr/lib/systemd/system/${service_name}.service":
             content => template($service_systemd_template),
             require => File[$service_config],
