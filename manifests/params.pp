2d0b98d27f73165bee744b1bd346d8b611e3616c
Alexander Ursu <alexander.ursu@gmail.com>
2018-08-23 12:21:09 +0200

    Added comments and description

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,6 @@
 class artifactory::params {
 
     # installation assets and settings
-    $oracle_java_package = 'jdk1.8'
     $oracle_java_home    = '/usr/java/default'
     $repo_location       = 'https://jfrog.bintray.com/artifactory-rpms'
     $package_name        = 'jfrog-artifactory-oss'
@@ -12,13 +11,26 @@ class artifactory::params {
     $minnbprocess        = 1024
 
     # installation and operational paths
+    # https://www.jfrog.com/confluence/display/RTF/Installing+on+Linux+Solaris+or+Mac+OS#InstallingonLinuxSolarisorMacOS-ManagedFilesandFolders
     $jfrog_home          = '/opt/jfrog'
     $install_dir         = "${jfrog_home}/artifactory"
+
+    # Tomcat home: /opt/jfrog/artifactory/tomcat
     $tomcat_home         = "${install_dir}/tomcat"
+
     $tomcat_webapps      = "${tomcat_home}/webapps"
+
+    # Artifactory etc: /etc/opt/jfrog/artifactory
     $etc_dir             = "/etc${install_dir}"
+
     $var_dir             = "/var${jfrog_home}"
     $run_dir             = "${var_dir}/run"
+
+    # Artifactory home: /var/opt/jfrog/artifactory
     $artifactory_home    = "${var_dir}/artifactory"
+
+    # Artifactory logs: /var/opt/jfrog/artifactory/logs
+    $log_dir             = "${artifactory_home}/logs"
+
     $artifactory_pid     = "${run_dir}/artifactory.pid"
 }
